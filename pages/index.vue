68b6c229410e51e6cfbd157e6c3a998b480309e3
Jonathan Watson <jonathanw12@outlook.com>
2021-04-21 17:54:52 +0100

    Add basic buttery widget

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,33 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">widgets</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="w-screen h-screen bg-gradient-to-tr from-orange to-red">
+    <vue-draggable-resizable
+      :w="600"
+      :h="300"
+      :handles="['br']"
+      :active="true"
+      :prevent-deactivation="true"
+      :parent="true"
+      :draggable="false"
+      @resizing="() => (resizing = true)"
+      @resizestop="() => (resizing = false)"
+    >
+      <div v-if="resizing" class="w-full h-full z-10 absolute"></div>
+      <iframe
+        src="menu"
+        frameborder="0"
+        sandbox="allow-scripts allow-popups allow-top-navigation-by-user-activation allow-forms allow-same-origin"
+        class="w-full h-full"
+      ></iframe>
+    </vue-draggable-resizable>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      resizing: false,
+    }
+  },
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ pages/menu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="p-6 w-full h-full">
+    <div>
+      <div class="text-md mb-4 font-semibold">{{ weekday }} {{ meal }}</div>
+      <div
+        v-for="(menuItem, i) in menu"
+        :key="i"
+        class="text-sm my-2 p-1 border-2 border-notion-text dark:border-notion-text-dark rounded"
+      >
+        {{ menuItem }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      weekday: '',
+      meal: '',
+      menu: [],
+    }
+  },
+  async mounted() {
+    const { weekday, meal, menu } = await this.$axios.$get('/buttery/menu/')
+    this.weekday = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ][weekday]
+    this.meal = meal
+    this.menu = menu
+  },
+}
+</script>
