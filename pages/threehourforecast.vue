31f240d46ecb80c130cd7203f0cd93c0854119e2
Jonathan Watson <jonathanw12@outlook.com>
2021-04-28 23:42:01 +0100

    Add weather icon pack

---
+++ pages/threehourforecast.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-6 w-full h-full">
+    <div class="-mb-6">
+      <div class="text-md mb-3 font-semibold">Weather forecast</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import significantWeatherCodes from '~/significantweather.js'
+import exampleThreeHours from '~/examplethreehours'
+
+export default {
+  data() {
+    return {
+      rawData: exampleThreeHours,
+    }
+  },
+  async mounted() {},
+  methods: {
+    getWeather(code) {
+      return significantWeatherCodes[code]
+    },
+  },
+}
+</script>
+
+<style scoped src="@/assets/css/weather-icons.min.css" />
