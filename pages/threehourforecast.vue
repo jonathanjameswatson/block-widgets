7351883abefcae011400e5248ff4255487c1ab39
Jonathan Watson <jonathanw12@outlook.com>
2021-04-29 00:29:26 +0100

    Add icon and location rendering

--- pages/threehourforecast.vue
+++ pages/threehourforecast.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="p-6 w-full h-full">
     <div class="-mb-6">
-      <div class="text-md mb-3 font-semibold">Weather forecast</div>
-      <i class="wi wi-night-sleet"></i>
+      <div class="text-md mb-3 font-semibold">{{ locationName }} weather</div>
+      <div v-for="(item, i) in forecast" :key="i">
+        <i class="wi text-4xl" :class="[`wi-${item.icon}`]"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -13,10 +15,26 @@ import exampleThreeHours from '~/examplethreehours'
 
 export default {
   data() {
+    const { features } = exampleThreeHours
+    const [feature] = features
+    const { properties } = feature
+    const { location, timeSeries } = properties
+    const { name } = location
+    const [shortenedName] = name.split(',')
+
+    const forecast = timeSeries.slice(0, 3).map((x) => {
+      const { significantWeatherCode } = x
+      const { icon } = this.getWeather(significantWeatherCode)
+      return {
+        icon,
+      }
+    })
+
     return {
       longitude: 52.2018,
       latitude: 0.1144,
-      rawData: exampleThreeHours,
+      locationName: shortenedName,
+      forecast,
     }
   },
   async mounted() {},
