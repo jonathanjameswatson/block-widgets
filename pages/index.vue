87c3adc701d5d17c72d9a91261786609c86e9b19
Jonathan Watson <jonathanw12@outlook.com>
2021-04-29 01:24:38 +0100

    Add title and buttons to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="w-screen h-screen bg-gradient-to-tr from-orange to-red">
+    <h1 class="extravagant-title">
+      https://jjw-widgets.netlify.app/{{ page }}
+    </h1>
+    <button class="extravagant-button" @click="() => (page = 'menu')">
+      Buttery menu
+    </button>
+    <button
+      class="extravagant-button"
+      @click="() => (page = 'threehourforecast')"
+    >
+      Three hour forecast
+    </button>
+
+    <br /><br />
+
     <vue-draggable-resizable
       :w="600"
       :h="300"
@@ -13,7 +28,7 @@
     >
       <div v-if="resizing" class="w-full h-full z-10 absolute"></div>
       <iframe
-        src="menu"
+        :src="page"
         frameborder="0"
         sandbox="allow-scripts allow-popups allow-top-navigation-by-user-activation allow-forms allow-same-origin"
         class="w-full h-full"
@@ -26,8 +41,37 @@
 export default {
   data() {
     return {
+      page: 'menu',
       resizing: false,
     }
   },
 }
 </script>
+
+<style scoped lang="postcss">
+.extravagant-title {
+  @apply transform-gpu text-6xl font-extrabold p-6 pt-0 pl-2 bg-clip-text text-transparent bg-gradient-to-br from-blue-400 to-blue-600;
+
+  animation-name: spin;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-duration: 5s;
+  text-align: center;
+}
+
+.extravagant-button {
+  @apply bg-blue-500 hover:bg-blue-700 text-white rounded py-2 px-4 font-bold mr-2;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(-90deg);
+  }
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,19 @@ export default {
   purge: [],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    colors: {
-      orange: '#FA8B3D',
-      red: '#F04B43',
-      'notion-bg': 'white',
-      'notion-text': 'rgb(55, 53, 47)',
-      'notion-bg-dark': 'rgb(47, 52, 55)',
-      'notion-text-dark': 'rgb(235, 235, 235)',
-    },
     fontFamily: {
       serif:
         'Lyon-Text, Georgia, YuMincho, "Yu Mincho", "Hiragino Mincho ProN", "Hiragino Mincho Pro", "Songti TC", "Songti SC", "SimSun", "Nanum Myeongjo", NanumMyeongjo, Batang, serif',
     },
     extend: {
+      colors: {
+        orange: '#FA8B3D',
+        red: '#F04B43',
+        'notion-bg': 'white',
+        'notion-text': 'rgb(55, 53, 47)',
+        'notion-bg-dark': 'rgb(47, 52, 55)',
+        'notion-text-dark': 'rgb(235, 235, 235)',
+      },
       lineHeight: {
         'notion-inner': 1.3,
         'notion-outer': 1.6,
