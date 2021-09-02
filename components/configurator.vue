4ab3976e7ae8a213df8dbc6c7b6c3a24a34e778f
Jonathan Watson <jonathanw12@outlook.com>
2021-09-02 12:24:09 +0100

    Add configurator controls

--- components/configurator.vue
+++ components/configurator.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
     <p v-for="property in properties" :key="property.propertyKey">
-      {{ property.name }}
+      <span class="text-blue-800 font-bold mr-2">{{ property.name }}:</span>
+      <template v-if="property.parameter.type === 'union'">
+        <blue-select
+          :options="property.parameter.options"
+          :value="mutableConfiguration[property.propertyKey]"
+          @input="updateValue(property.propertyKey, $event)"
+        />
+      </template>
+      <template v-else-if="property.parameter.type === 'string'">
+        <input
+          class="
+            inline-block
+            font-bold
+            bg-blue-500
+            hover:bg-blue-700
+            rounded
+            p-2
+            focus:outline-none focus:shadow-outline
+            z-20
+          "
+          :value="mutableConfiguration[property.propertyKey]"
+          @input="updateValue(property.propertyKey, $event.target.value)"
+        />
+      </template>
+
+      <br /><br />
     </p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref, computed } from '@vue/composition-api'
 
 import Configuration, {
   getEditableProperties,
@@ -17,17 +42,27 @@ import Configuration, {
 export default defineComponent({
   props: {
     configuration: {
-      type: Configuration,
+      type: Object as () => Configuration,
       required: true,
     },
   },
   setup(props) {
-    const { configuration } = props
-    const propertyNames = getEditableProperties(configuration)
-    const properties = propertyNames.map((propertyName) =>
-      getPropertyMetadata(configuration, propertyName)
+    const mutableConfiguration = ref(props.configuration)
+    const propertyNames = computed(() =>
+      getEditableProperties(mutableConfiguration.value)
     )
-    return { properties }
+    const properties = computed(() => {
+      return propertyNames.value.map((propertyName) =>
+        getPropertyMetadata(mutableConfiguration.value, propertyName)
+      )
+    })
+
+    const updateValue = (propertyKey: string, propertyValue: string) => {
+      // @ts-ignore
+      mutableConfiguration.value[propertyKey] = propertyValue
+    }
+
+    return { mutableConfiguration, propertyNames, properties, updateValue }
   },
 })
 </script>
