4e66e500114da5315b27d4b3339fae9fd728c67b
Jonathan Watson <jonathanw12@outlook.com>
2021-09-03 18:23:07 +0100

    Add color theme control

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,27 @@
   <nuxt class="mx-auto p-4" />
 </template>
 
+<script lang="ts">
+import {
+  defineComponent,
+  inject,
+  useContext,
+  Ref,
+  onMounted,
+} from '@nuxtjs/composition-api'
+import Configuration from '~/ts/configuration'
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      const configuration = inject('configuration') as Ref<Configuration>
+      const { $colorMode } = useContext()
+      $colorMode.preference = configuration.value.theme.toLowerCase()
+    })
+  },
+})
+</script>
+
 <style lang="postcss">
 body {
   @apply text-notion-text bg-notion-bg dark:text-notion-text-dark dark:bg-notion-bg-dark font-serif;
