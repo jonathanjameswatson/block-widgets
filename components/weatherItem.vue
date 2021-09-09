d069f590463fd492564c4a1c947ab45e8e654d09
Jonathan Watson <jonathanw12@outlook.com>
2021-09-09 10:56:00 +0100

    Remove weather.css

--- components/weatherItem.vue
+++ components/weatherItem.vue
@@ -17,4 +17,27 @@ export default defineComponent({
 })
 </script>
 
-<style scoped src="@/assets/css/weather.css" />
+<style scoped>
+@import 'weather-icons/css/weather-icons.css';
+
+@font-face {
+  font-family: 'weathericons2';
+  src: url('~/node_modules/weather-icons/font/weathericons-regular-webfont.eot');
+  src: url('~/node_modules/weather-icons/font/weathericons-regular-webfont.eot?#iefix')
+      format('embedded-opentype'),
+    url('~/node_modules/weather-icons/font/weathericons-regular-webfont.woff2')
+      format('woff2'),
+    url('~/node_modules/weather-icons/font/weathericons-regular-webfont.woff')
+      format('woff'),
+    url('~/node_modules/weather-icons/font/weathericons-regular-webfont.ttf')
+      format('truetype'),
+    url('~/node_modules/weather-icons/font/weathericons-regular-webfont.svg#weather_iconsregular')
+      format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.wi {
+  font-family: 'weathericons2' !important;
+}
+</style>
