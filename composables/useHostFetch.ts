2383c5071d8fbbcfdfe64a0781d27a8d481eaa03
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-04-21 18:25:50 +0100

    Add absolute URL support

--- composables/useHostFetch.ts
+++ composables/useHostFetch.ts
@@ -3,6 +3,20 @@ import { $fetch } from 'ohmyfetch'
 export const useHostFetch = () => {
   const hostUrl = useRuntimeConfig().hostUrl
   return $fetch.create({
-    baseURL: `${hostUrl}/api`,
+    async onRequest({ request, options }) {
+      if (options.baseURL === undefined) {
+        let url = ''
+
+        if (typeof request === 'string') {
+          url = request
+        } else {
+          url = request.url
+        }
+
+        if (!url.startsWith('http')) {
+          options.baseURL = `${hostUrl}/api`
+        }
+      }
+    },
   })
 }
